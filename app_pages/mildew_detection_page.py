336ecd3bad5204235ba231d02c78503e661df62d
fatimamahate <fatimamahate@yahoo.co.uk>
2024-03-04 17:17:41 +0000

    Create mildew detection app page and ml performance page and add imports to each

---
+++ app_pages/mildew_detection_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+from src.data_management import download_dataframe_as_csv
+from src.machine_learning.predictive_analytics import (
+    load_model_and_predict,
+    resize_input_image,
+    plot_pred_prob
+)
