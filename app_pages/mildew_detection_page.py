743348f05b851b688b8e968e7738b0a434b109d2
fatimamahate <fatimamahate@yahoo.co.uk>
2024-03-04 18:26:47 +0000

    complete main code of mildew detection page

--- app_pages/mildew_detection_page.py
+++ app_pages/mildew_detection_page.py
@@ -25,4 +25,38 @@ def mildew_detection_page_body():
     st.info(
         f'Upload an image of a cherry leaf for predictions (more than one is permitted)'
     )
-    
+    img_buffer=st.file_uploader(' ', type=['png', 'jpg'], accept_multiple_files=True)
+    button_for_pred = st.button('Predict!')
+
+    if button_for_pred:
+        make_pred(img_buffer)
+
+def make_pred(img_buffer):
+    if img_buffer is not None:
+        df_report = pd.DataFrame([])
+        for img in img_buffer:
+            img_pil = (Image.open(img))
+            st.info(f'Cherry Leaf: **{img.name}**')
+            img_array = np.array(img_pil)
+            st.image(
+                img_pil,
+                caption=f'Image Size (width x height): {img_array.shape[1]}px x {img_array.shape[0]}px'
+            ) 
+            version = 'v1'
+            resized_img=resize_input_image(img=img_pil, version=version)
+            pred_prob, pred_class = load_model_and_predict(resized_img,version=version)
+            plot_pred_prob(pred_prob=pred_prob, pred_class=pred_class)
+            df_report=df_report.append(
+                {
+                    'Name':image.name,
+                    'Result': pred_class
+                },
+                ignore_index=True
+            )
+    if not df_report.empty:
+        st.success('Analysis Report')
+        st.table(df_report)
+        st.markdown(
+            download_dataframe_as_csv(df_report),
+            unsafe_allow_html=True
+        )  
